5cb37ebf78bb882312489b946b9a7aaa840fa947
Cristea40 <catalincristea40@yahoo.com>
2023-11-22 21:15:09 +0200

    Astea sunt

---
+++ Hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Animal {
+public:
+    // Constructor
+    Animal(int legs) : numLegs(legs) {}
+};
+
+int main(){
+
+  Animal Caine(4);
+
+
+  std::cout<<Animal;
+
+ 
+
+ 
+
+
+return 0;
+}
+
